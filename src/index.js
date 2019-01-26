e936900abf88bf7be47ba360aac6fbb6e85532d2
Slavik <slavik93mj@gmail.com>
2019-01-26 14:49:30 +0200

    added alert to insex.js file

--- src/index.js
+++ src/index.js
@@ -1,70 +1,48 @@
 import './index.css';
 
-const renderRow = (arr) =>
-    `<div class='square'>${arr.join('</div><div class=\'square\'>')}</div>`
+const renderRow = arr =>
+    `<div class='square'>${arr.join('</div><div class=\'square\'>')}</div>`;
 
-const renderRows = (arr) => {
-    return arr.reduce((acc, current) => {
-        return acc + renderRow(current);
-    }, '');
-};
+const renderRows = arr => arr.reduce((acc, current) => acc + renderRow(current), '');
 
 const shuffleState = (state) => {
     const suffleArr = (inputArr) => {
         const arr = [...inputArr];
-        for (var j, x, i = arr.length; i; j = parseInt(Math.random() * i), x = arr[--i], arr[i] = arr[j], arr[j] = x);
+        for (let j, x, i = arr.length; i; j = parseInt(Math.random() * i), x = arr[--i], arr[i] = arr[j], arr[j] = x);
         return arr;
-    }
-    const flattenArr = state.reduce((acc, val) => {
-        return [...acc, ...val]
-    }, []);
+    };
+    const flattenArr = state.reduce((acc, val) => [...acc, ...val], []);
     const shuffledArr = suffleArr(flattenArr);
     return [
         shuffledArr.slice(0, 4),
         shuffledArr.slice(4, 8),
         shuffledArr.slice(8, 12),
         shuffledArr.slice(12, 16),
-    ]
-}
+    ];
+};
 
 const renderGame = (shuffledArray, domNode = document.getElementById('app')) => {
     const rows = renderRows(shuffledArray).replace(
         '<div class=\'square\'></div>',
-        '<div class=\'square empty\'></div>'
+        '<div class=\'square empty\'></div>',
     );
     domNode.innerHTML = rows;
 };
-
-
-
-
-
-
-
 const fifteen = [
     [1, 2, 3, 4],
     [5, 6, 7, 8],
     [9, 10, 11, 12],
-    [13, 14, 15, null]
+    [13, 14, 15, null],
 ];
-
-
 const state = shuffleState(fifteen);
 renderGame(fifteen);
-
-if (renderGame(fifteen) === fifteen) { alert('you won') }
-
 const getXY = (source) => {
     const y = source.findIndex(arr => arr.includes(null));
     const x = source[y].findIndex(x => x === null);
-    console.log({ y, x });
     return { y, x };
 
 };
-//const xy = getXY(state);
-
-
-document.addEventListener('keydown', e => {
+document.addEventListener('keydown', (e) => {
     const xyOfEmptyCell = getXY(state);
     if (e.keyCode === 38) {
         // up arrow
@@ -105,3 +83,8 @@ document.addEventListener('keydown', e => {
     }
 
 });
+
+if (renderGame(state) === renderGame(fiftten)) {
+    alert('you won,press "OK" to play again!');
+    renderGame(state)
+}
